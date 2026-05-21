c073a06ed1036d90dfbda639a4cdd7f546c5e275
AadiC11 <133283405+AadiC11@users.noreply.github.com>
2026-05-21 12:47:47 -0700

    Add files via upload

---
+++ app/a5/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { addToast } from "@heroui/toast";
+import { title } from "@/components/primitives";
+import { Button } from "@heroui/button";
+// import { Tag } from "lucide-react";
+
+export default function A5page() {
+  const reason1Toast = () => {
+    addToast({
+    title: "#1",
+    description:
+    "He was dropped into a vat of acid and became crazy",
+    color: "blue",
+    });}
+    const reason2Toast = () => {
+      addToast({
+      title: "#2",
+      description:
+      "In iterations of the story like the 2019 film, the joker was forced into his mindset by societal abuse",
+      color: "blue",
+      });}
+      const reason3Toast = () => {
+        addToast({
+        title: "#3",
+        description:
+        "The joker hurt alot of people but he did it to drive Batman insane, not primarily for the fun of harming people",
+        color: "blue",
+        });}
+    
+  return (
+    <>
+      <h1 className={title()}>A5: Defend the fictional villan</h1>
+      <ul><b>Three reasons why joker wasn't completely evil:</b></ul>
+      <Button
+        onPress={() => reason1Toast()}>Reason #1</Button>
+        <Button
+        onPress={() => reason2Toast()}>Reason #2</Button>
+        <Button
+        onPress={() => reason3Toast()}>Reason #3</Button>
+      
+      </>
+
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,83 @@
+"use client";
+import {Image} from "@heroui/image";
+import { useState } from "react";
+import { Snippet } from "@heroui/snippet";
+import { Code } from "@heroui/code";
+import { Button } from "@heroui/button";
+
+import { title, subtitle } from "@/components/primitives";
+import ConfettiButton from "@/components/confettiButton";
+
+export default function Home() {
+  const [spinning, setSpinning] = useState(false);
+
+  const spinButton = () => {
+    setSpinning(true);
+    setTimeout(() => setSpinning(false), 1500);
+  };
+
+  return (
+    // <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+    //   <div className="inline-block max-w-xl text-center justify-center">
+    //     <span className={title()}>Discount&nbsp;</span>
+    //     <span className={title({ color: "violet" })}>Time Machine&nbsp;</span>
+    //     <br />
+    //     <span className={title()}>Wholesale</span>
+    //     <div className={subtitle({ class: "mt-4" })}>
+    //       A project template for YOU to make your own!
+    //     </div>
+    //   </div>
+
+    //   <div className="flex gap-3">
+    //     <ConfettiButton />
+
+    //     <div className={spinning ? "animate-spin" : ""}>
+    //       <Button radius="full" variant="ghost" onPress={spinButton}>
+    //         Spin
+    //       </Button>
+    //     </div>
+    //   </div>
+
+    //   <div className="mt-8">
+    //     <Snippet hideCopyButton hideSymbol variant="bordered">
+    //       <span>
+    //         Get started by editing <Code color="primary">app/page.tsx</Code>
+    //       </span>
+    //     </Snippet>
+    //   </div>
+    // </section>
+    <div>
+      <Image
+        alt="sppoky joker"
+        src="https://myhotposters.com/cdn/shop/products/mF0157_09105c0b-9f70-4778-af6c-a15b8b02b9cd_1024x1024.jpeg?v=1748544265"
+        width={100}
+        >
+      </Image>
+      <Image
+        alt="live action joker"
+        src="https://tse1.mm.bing.net/th/id/OIP.TOM7XWyinuwUKpziY1_kDgHaNK?w=115&h=180&c=7&r=0&o=7&pid=1.7&rm=3"
+        width={100}
+        >
+      </Image>
+      <Image
+        alt="comic joker"
+        src="https://tse3.mm.bing.net/th/id/OIP.Et-W72DV7_WTq0irVuPwTQHaL-?w=202&h=315&c=7&r=0&o=7&pid=1.7&rm=3"
+        width={100}
+        >
+      </Image>
+      <Image
+        alt="batman who laughs"
+        src="https://tse2.mm.bing.net/th/id/OIP.H2yBZDZMXeBgraWpZFvtdAHaLS?w=202&h=308&c=7&r=0&o=7&pid=1.7&rm=3"
+        width={100}
+        >
+      </Image>
+      <Image
+        alt="the killing joke"
+        src="https://tse4.mm.bing.net/th/id/OIP.I5ghLnNTNLmS9WOV72PIDwAAAA?rs=1&pid=ImgDetMain&o=7&rm=3"
+        width={100}
+        >
+      </Image>
+      
+    </div>
+  );
+}
